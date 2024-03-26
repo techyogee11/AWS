663819881dc160b193110e5320e91b32b35c8fd4
Yogesh Gulve <Yogesh_Gulve@epam.com>
2024-03-26 19:48:21 +0530

    SNS Sample with AWS Lambda

---
+++ SNS-Sample/SNS-Sample/SNS-Sample/Controllers/OrderController.cs
@@ -0,0 +1,30 @@
+﻿using Amazon.Runtime;
+using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace SNS_Sample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        [HttpPost]
+        public async Task ProcessOrderAsync(Order order)
+        {
+            var credentials = new BasicAWSCredentials("AKIA3FNPHSP2BEQPWHQF", "JCKYzLdeI7HytIIyvRIzAeNLf3xIrj/MPTK4Zs8x");
+            var client = new AmazonSimpleNotificationServiceClient(credentials, Amazon.RegionEndpoint.USEast1);
+
+            var request = new PublishRequest()
+            {
+                TopicArn = "arn:aws:sns:us-east-1:767556031476:IncomingOrder",
+                Message = JsonSerializer.Serialize(order),
+                Subject = "New Order Received - " + Guid.NewGuid().ToString()
+            };
+
+            var response = await client.PublishAsync(request);
+            
+        }
+    }
+}
--- SampleLambda/obj/Debug/net8.0/SampleLambda.AssemblyInfo.cs
+++ SampleLambda/obj/Debug/net8.0/SampleLambda.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SampleLambda")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22ccfe7603449e8166aac5058e2bb7d752c4fa1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SampleLambda")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SampleLambda")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
