3feb5a08a1f9aa2ab875a63cfc118aec9989d78c
Yogesh Gulve <Yogesh_Gulve@epam.com>
2024-04-08 18:11:54 +0530

    DynamoDB sample

---
+++ DynamoStudentMananger/Controllers/StudentController.cs
@@ -0,0 +1,51 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using DynamoStudentMananger.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamoStudentMananger.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class StudentController : Controller
+    {
+        private readonly IDynamoDBContext _dbContext;
+        public StudentController(IDynamoDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        [HttpGet("{studentId}")]
+        public async Task<IActionResult> GetById(int studentId) 
+        {
+            var student = await _dbContext.LoadAsync<Student>(studentId);
+            if(student == null) return NotFound();
+            return Ok(student);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllStudent()
+        {
+            var student = await _dbContext.ScanAsync<Student>(default).GetRemainingAsync();
+            return Ok(student);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateResult(Student studentRequest)
+        {
+            var student = await _dbContext.LoadAsync<Student>(studentRequest.Id);
+            if (student != null) return BadRequest($"Student with Id {student.Id} Already Exists");
+            await _dbContext.SaveAsync(studentRequest);
+            return Ok(studentRequest);
+        }
+
+        [HttpDelete("{studentId}")]
+        public async Task<IActionResult> DeleteStudent(int studentId)
+        {
+            var student = await _dbContext.LoadAsync<Student>(studentId);
+            if(student == null) return NotFound();  
+            await _dbContext.DeleteAsync(student);
+            return NoContent();
+        }
+    }
+}
---
+++ DynamoStudentMananger/Readme.md
@@ -0,0 +1,27 @@
+## DynamoDB
+
+1. DynamoDB is a fully managed NoSQL database service provided by Amazon Web Services (AWS).
+
+2. It provides fast and predictable performance with seamless scalability.
+
+3. DynamoDB allows users to create databases capable of storing and retrieving any amount of data, and serving any level of request traffic.
+
+4. It automatically spreads the data and traffic for a table over a sufficient number of servers to manage capacity and performance.
+
+5. With DynamoDB, you can create database tables that can store and retrieve any amount of data and serve any level of request traffic.
+
+6. DynamoDB supports both key-value and document data models.
+
+7. It offers encryption at rest, which provides an additional layer of data protection by securing your data from unauthorized access to the underlying storage.
+
+8. It also supports automated backups and point-in-time recovery to safeguard your data.
+
+9. One can use DynamoDB to create a database table that can store and retrieve any amount of data, and serve any level of request traffic.
+
+10. DynamoDB integrates well with other AWS services like AWS Lambda, Amazon Cognito, AWS Glue, etc.
+
+11. DynamoDB provides on-demand backup capability, enabling full backups of your tables for long-term retention, and helping you meet corporate and governmental regulatory requirements.
+
+12. It provides built-in security with AWS Identity and Access Management (IAM), enabling granular control on admin and end-user access.
+
+13. DynamoDB is serverless with no servers to provision, patch, or manage, and no software to install, maintain, or operate.
