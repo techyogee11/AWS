bf8ac2524af2ffbdb07855e36eed29cc4e0a704d
Yogesh Gulve <Yogesh_Gulve@epam.com>
2024-04-17 16:02:11 +0530

    AWS API Gateway

---
+++ APIGateway/StudentManagement/Function.cs
@@ -0,0 +1,50 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using Newtonsoft.Json;
+using StudentManagement.Models;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace StudentManagement;
+
+public class Function
+{
+
+    public async Task<List<Student>> GetAllStudentAsync(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+    {
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        DynamoDBContext dynamoDBContext = new DynamoDBContext(client);  
+        var data = await dynamoDBContext.ScanAsync<Student>(default).GetRemainingAsync();
+        
+        return data;
+    }
+
+    public async Task<APIGatewayHttpApiV2ProxyResponse> CreateStudentAsync(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+    {
+        var studentRequest = JsonConvert.DeserializeObject<Student>(request.Body);
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        DynamoDBContext dbContext = new DynamoDBContext(client);
+        await dbContext.SaveAsync(studentRequest);
+        var message = $"Student with Id {studentRequest?.Id} Created";
+        LambdaLogger.Log(message);
+        return new APIGatewayHttpApiV2ProxyResponse
+        {
+            Body = message,
+            StatusCode = 200
+        };
+    }
+    public async Task<Student> GetStudentByIdAsync(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+    {
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        DynamoDBContext dbContext = new DynamoDBContext(client);
+        string idFromPath = request.PathParameters["id"];
+        int id = Int32.Parse(idFromPath);
+        var student = await dbContext.LoadAsync<Student>(id);
+        if (student == null) throw new Exception("Not Found!");
+        return student;
+    }
+
+}
---
+++ APIGateway/StudentManagement/Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Models
+{
+    [DynamoDBTable("students")]
+    public class Student
+    {
+        [DynamoDBHashKey("id")]
+        public int? Id { get; set; }
+        [DynamoDBProperty("first_name")]
+        public string? FirstName { get; set; }
+        [DynamoDBProperty("last_name")]
+        public string? LastName { get; set; }
+        [DynamoDBProperty("class")]
+        public int Class { get; set; }
+    }
+}
