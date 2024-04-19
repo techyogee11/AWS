1647082fb8b42e64495fa68e2f2506830d1bbd38
Yogesh Gulve <Yogesh_Gulve@epam.com>
2024-04-19 18:20:27 +0530

    Authorization code updated

---
+++ APIGateway/AuthLambda/Function.cs
@@ -0,0 +1,90 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using AuthLambda.Models;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace AuthLambda;
+
+public class Function
+{
+
+
+    private const string key = "43443FDFDF34DF34343fdf344SDFSDFSDFSDFSDF4545354345SDFGDFGDFGDFGdffgfdGDFGDGR%";
+    public APIGatewayCustomAuthorizerResponse ValidateTokenAsync(APIGatewayCustomAuthorizerRequest request, ILambdaContext context)
+    {
+        var authToken = request.Headers["authorization"];
+
+        var claimsPrincipal = GetClaimsPrincipal(authToken);
+        var effect = claimsPrincipal == null ? "Deny" : "Allow";
+        var principalId = claimsPrincipal == null ? "401" : claimsPrincipal?.FindFirst(ClaimTypes.Name)?.Value;
+        return new APIGatewayCustomAuthorizerResponse()
+        {
+            PrincipalID = principalId,
+            PolicyDocument = new APIGatewayCustomAuthorizerPolicy()
+            {
+                Statement = new List<APIGatewayCustomAuthorizerPolicy.IAMPolicyStatement>
+            {
+                new APIGatewayCustomAuthorizerPolicy.IAMPolicyStatement()
+                {
+                    Effect = effect,
+                    Resource = new HashSet<string> { "arn:aws:execute-api:ap-south-1:821175633958:sctmtm1ge8/*/*" },
+                    Action = new HashSet<string> { "execute-api:Invoke" }
+                }
+            }
+            }
+        };
+    }
+
+    private ClaimsPrincipal GetClaimsPrincipal(string authToken)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var validationParams = new TokenValidationParameters()
+        {
+            ValidateLifetime = true,
+            ValidateAudience = false,
+            ValidateIssuer = false,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
+        };
+        try
+        {
+            return tokenHandler.ValidateToken(authToken, validationParams, out SecurityToken securityToken);
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
+
+    public async Task<string> GenerateTokenAsync(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+    {
+        var tokenRequest = JsonConvert.DeserializeObject<User>(request.Body);
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient();
+        DynamoDBContext dbContext = new DynamoDBContext(client);
+
+        //check if user exists in ddb
+        var user = await dbContext.LoadAsync<User>(tokenRequest?.Email);
+        if (user == null) throw new Exception("User Not Found!");
+        if (user.Password != tokenRequest.Password) throw new Exception("Invalid Credentials!");
+
+        var token = GenerateJWT(user);
+        return token;
+    }
+    public string GenerateJWT(User user)
+    {
+        var claims = new List<Claim> { new(ClaimTypes.Email, user.Email), new(ClaimTypes.Name, user.UserName) };
+        byte[] secret = Encoding.UTF8.GetBytes(key);
+        var signingCredentials = new SigningCredentials(new SymmetricSecurityKey(secret), SecurityAlgorithms.HmacSha256);
+        var token = new JwtSecurityToken(claims: claims, expires: DateTime.UtcNow.AddMinutes(5), signingCredentials: signingCredentials);
+        var tokenHandler = new JwtSecurityTokenHandler();
+        return tokenHandler.WriteToken(token);
+    }
+}
---
+++ APIGateway/AuthLambda/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthLambda.Models
+{
+    [DynamoDBTable("users")]
+    public class User
+    {
+        [DynamoDBHashKey("email")]
+        public string? Email { get; set; }
+
+        [DynamoDBProperty("username")]
+        public string? UserName { get; set; }
+
+        [DynamoDBProperty("password")]
+        public string? Password { get; set; }
+    }
+}
